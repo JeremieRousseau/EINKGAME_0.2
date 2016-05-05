2217740f03b2f8f797b031371ba48e2621ee05e3
JeremieRousseau <jeremie.rousseau@orange.fr>
2016-05-05 16:18:43 +0200

    Error 404 file.

---
+++ JS/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>ERROR 404</title>
+		<meta name="author" content="Rousseau J&eacute;r&eacute;mie" >
+		<meta name="robots" content="index, follow" ><link rel="shortcut icon" href="../favicon_all.ico" >
+		<link  rel="stylesheet" media="screen" type="text/css" title="template" href="../CSS/Index404Tpl.css" >
+	</head>
+
+<body>
+	<h1>ERROR 404</h1>
+	<p><?php echo "You cannot visit this directory." ?></p>
+</body>
+</html>
