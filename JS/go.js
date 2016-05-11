3cfed65e2e05c1fbd1fca71d8ae1683db810239f
JeremieRousseau <jeremie.rousseau@orange.fr>
2016-05-11 13:58:47 +0200

    a begin of pathfinding with walls

--- JS/go.js
+++ JS/go.js
@@ -2,54 +2,143 @@
 //a first jet.
 //because border of the map is not implemented.
 
+
+
 	function goRight(){
+		console.log("goR");
 		Id( goX + v + goY ).className = "floor";
 		goX += 1;
 		goY += 0;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX -= 1;
+					goY += 0;
+					console.log("wall");
+					GOTO = "DiaDownRight";
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	
 	function goDiaUpRight(){
+		console.log("goDiaUR");
 		Id( goX + v + goY ).className = "floor";
 		goX += 1;
 		goY += 1;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX -= 1;
+					goY -= 1;
+					GOBACKWALL = true;
+					console.log("wall");
+					GOTO = "DiaUpLeft";
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	
 	function goDiaDownRight(){
+		console.log("goDiaR");
 		Id( goX + v + goY ).className = "floor";
 		goX += 1;
 		goY -= 1;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX -= 1;
+					goY += 1;		
+					console.log("wall");
+				//	GOTO = "DiaDownLeft";
+
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	
+		
 	function goLeft(){
+	console.log("goL");
 		Id( goX + v + goY ).className = "floor";
 		goX -= 1;
 		goY += 0;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX += 1;
+					goY += 0;
+					console.log("wall");
+					console.log(GOTO);
+					GOTO = "DiaDownLeft";
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	function goDiaUpLeft(){
+		console.log("goDiaUL");
 		Id( goX + v + goY ).className = "floor";
 		goX -= 1;
-		goY += 1;
+		goY += 1; 
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX += 1;
+					goY -= 1;
+					console.log("wall");
+					GOTO = "Left";
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	function goDiaDownLeft(){
+		console.log("goDiaD");
 		Id( goX + v + goY ).className = "floor";
 		goX -= 1;
 		goY -= 1;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX += 1;
+					goY += 1;
+					console.log("wall");
+					GOTO = "Down";
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
 	function goUp(){
+		console.log("goU");
 		Id( goX + v + goY ).className = "floor";
 		goX += 0;
 		goY += 1;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX += 0;
+					goY -= 1;
+					console.log("wall");
+					GOTO = "DiaUpRight";
+
+        }else{
+			GOTO = "";
+		}
+		
 		Id( goX + v + goY ).className = "unit";
 	}
 	function goDown(){
+		console.log("goD");
 		Id( goX + v + goY ).className = "floor";
-		goX += ;
+		goX += 0;
 		goY -= 1;
+		block = Id( goX + v + goY ).className;
+		if ( block == "wall") {
+            		goX += 0;
+					goY += 1;
+					console.log("wall");
+					GOTO = "DiaDownRight";
+
+        }else{
+			GOTO = "";
+		}
 		Id( goX + v + goY ).className = "unit";
 	}
