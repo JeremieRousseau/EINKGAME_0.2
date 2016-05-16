0fbccd3167ac2df983949eac69d405077df3b25b
JeremieRousseau <jeremie.rousseau@orange.fr>
2016-05-16 10:52:29 +0200

    A star

---
+++ JS/DUR.js
@@ -0,0 +1,27 @@
+	function mDUR(){
+
+if (dur() != true) {
+
+	if (u() != true) {
+
+		if (r() != true) {
+
+			if (dul() != true) {
+
+				if (l() != true) {
+
+					if (ddr() != true) {
+
+						if (d() != true) {
+
+							if (ddl() != true) {
+								clearInterval(clock);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+}
