6181f1cfa240ca2f59145ec50ba09abd0dbdb5e5
JeremieRousseau <jeremie.rousseau@orange.fr>
2016-05-16 11:03:38 +0200

    i changed the grid to have X-Y code for each case 10 on top and 1 in bottom , 1 on left, and 10 on right

--- JS/grid.js
+++ JS/grid.js
@@ -1,19 +1,25 @@
 function GRIDCREATED(){
 
-//JS var no used :: SIZE  = prompt("Please enter the size of the canvas", "20");
+//JS var no used :
+//                   SIZE  = prompt("Please enter the size of the canvas", "20");
 //JS future idea configure SIZE of the board  if we know the size of the screen ??
-SIZE = 20;
+
+//SIZe 10 because it's better to work with Komodo IDE
+SIZE = 10;
 
 var div = Id("master");
 
-for(var j = 1; j <= SIZE; j++)
+
+// X = i so the fist case on top  are 1-10 after 2-10
+//Y = j the bottom case on left is 10-1;
+for(var j = 10; j ; j--)
 {
 	for(var i = 1; i <= SIZE; i++)
     {
 		var p  = document.createElement("P");
 		//var t =  document.createTextNode("P");
 		//p.appendChild(t);
-		p.setAttribute("id", j + "-" + i);
+		p.setAttribute("id",  i + "-" + j);
 		p.setAttribute("onClick", "Select(this.id)");
 		p.setAttribute("class", "empty");
 		div.appendChild(p);
@@ -23,4 +29,13 @@ for(var j = 1; j <= SIZE; j++)
 	}
 	
 	Id("5-5").className = "unit";
+	
+	Id("5-6").className = "wall";
+	Id("5-7").className = "wall";
+	
+	Id("5-8").className = "wall";
+	Id("6-8").className = "wall";
+	Id("7-8").className = "wall";
+	
+ diffPath.push( "5-6", "5-7", "5-8", "5-9", "6-8", "7-8" );
 }
